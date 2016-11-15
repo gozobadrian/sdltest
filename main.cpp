5c17be2fdbf2e620b1cbac0402be465ff46e8a5e
Elev <gozobadrian@yahoo.ro>
2016-11-15 09:37:02 +0200

    Lesson 08 v2

--- main.cpp
+++ main.cpp
@@ -45,8 +45,82 @@ void close()
     SDL_Quit();
 }
 
+void moveHorizontalLine(int &lineY,bool &lineYDown)
+{
+    if(lineY==SCREEN_HEIGHT||lineY==0)
+    {
+        if(lineYDown)
+        {
+            lineYDown=false;
+        }
+        else
+        {
+                lineYDown=true;
+        }
+    }
+    if(lineY<SCREEN_HEIGHT&&lineYDown)
+    {
+        lineY++;
+    }
+    else
+    {
+        lineY--;
+    }
+}
+
+void moveVerticalDots(int &dotX, bool &dotXRight)
+{
+    if(dotX==SCREEN_WIDTH||dotX==0)
+    {
+        if(dotXRight)
+        {
+            dotXRight=false;
+        }
+        else
+        {
+            dotXRight=true;
+        }
+    }
+    if(dotX<SCREEN_WIDTH&&dotXRight)
+    {
+        dotX++;
+    }
+    else
+    {
+        dotX--;
+    }
+}
+
+void moveOutlineRect(int &x,int &y,int &xx,int &yy, bool &down)
+{
+    if(x==0)
+    {
+        down=true;
+    }
+    else if(x==SCREEN_WIDTH)
+    {
+        down=false;
+    }
+    if(down)
+    {
+        x++;y++;
+        xx-=2;yy-=2;
+    }
+    else
+    {
+       x--;y--;
+       xx+=2;yy+=2;
+    }
+}
+
 int main(int argc, char* args[])
 {
+    int lineY=0,dotX=0;
+    int outlineRectX=SCREEN_WIDTH/4;
+    int outlineRectY=SCREEN_HEIGHT/4;
+    int outlineRectXX=SCREEN_WIDTH/2;
+    int outlineRectYY=SCREEN_HEIGHT/2;
+    bool lineYDown=false,dotXRight=false,rectDown=false;
     if(!init())
     {
         printf("Failed to initialise\n");
@@ -69,22 +143,30 @@ int main(int argc, char* args[])
                 SDL_SetRenderDrawColor(gRender,0xFF,0xFF,0xFF,0xFF);
                 SDL_RenderClear(gRender);
 
+                ///Fill rect
                 SDL_Rect fillRect={SCREEN_WIDTH/4,SCREEN_HEIGHT/4,SCREEN_WIDTH/2,SCREEN_HEIGHT/2};
                 SDL_SetRenderDrawColor(gRender,0xFF,0x00,0x00,0xFF);
                 SDL_RenderFillRect(gRender,&fillRect);
 
-                SDL_Rect outlineRect={SCREEN_WIDTH/6,SCREEN_HEIGHT/6,SCREEN_WIDTH*2/3,SCREEN_HEIGHT*2/3};
+                ///Outline rect
+                SDL_Rect outlineRect={outlineRectX,outlineRectY,outlineRectXX,outlineRectYY};
                 SDL_SetRenderDrawColor(gRender,0x00,0xFF,0x00,0xFF);
                 SDL_RenderDrawRect(gRender,&outlineRect);
+                moveOutlineRect(outlineRectX,outlineRectY,outlineRectXX,outlineRectYY,rectDown);
 
+
+                ///Horizontal line
                 SDL_SetRenderDrawColor(gRender,0x00,0x00,0xFF,0xFF);
-                SDL_RenderDrawLine(gRender,0,SCREEN_HEIGHT/2,SCREEN_WIDTH,SCREEN_HEIGHT/2);
+                SDL_RenderDrawLine(gRender,0,lineY,SCREEN_WIDTH,lineY);
+                moveHorizontalLine(lineY,lineYDown);
 
-                SDL_SetRenderDrawColor(gRender,0xFF,0xFF,0x00,0xFF);
+                ///Vertical dots
+                SDL_SetRenderDrawColor(gRender,0x00,0x00,0xFF,0xFF);
                 for(int i=0;i<=SCREEN_HEIGHT;i+=2)
                 {
-                    SDL_RenderDrawPoint(gRender,SCREEN_WIDTH/2,i);
+                    SDL_RenderDrawPoint(gRender,dotX,i);
                 }
+                moveVerticalDots(dotX,dotXRight);
 
                 SDL_RenderPresent(gRender);
             }
